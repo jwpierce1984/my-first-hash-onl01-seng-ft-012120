c06d74e54ad741402180faadc8b4a2840418316b
Justin Pierce <jwpierce1984@gmail.com>
2020-02-03 13:40:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+ my_hash = {}
+ my_hash[name] = "Justin"
+ my_hash[birthday] = "March 5"
   
 end
 
